79e27e0039bd9b9bb3c9503cbe1abdbccdee189d
Martin Finkel <martin@videolabs.io>
2019-01-28 16:50:53 +0700

    WinForms sample: review feedback

--- Samples/LibVLCSharp.WinForms.Sample/Form1.Designer.cs
+++ Samples/LibVLCSharp.WinForms.Sample/Form1.Designer.cs
@@ -1,7 +1,4 @@
-﻿using LibVLCSharp.Shared;
-using System.Windows.Forms;
-
-namespace LibVLCSharp.WinForms.Sample
+﻿namespace LibVLCSharp.WinForms.Sample
 {
     partial class Form1
     {
@@ -22,47 +19,5 @@ namespace LibVLCSharp.WinForms.Sample
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        LibVLC _libVLC;
-        VideoView _videoView;
-        MediaPlayer _mp;
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            if(!DesignMode)
-                Core.Initialize();
-
-            _videoView = new VideoView();
-            ((System.ComponentModel.ISupportInitialize)(_videoView)).BeginInit();
-            SuspendLayout();
-
-            _libVLC = new LibVLC("-vv");
-            _mp = new MediaPlayer(_libVLC);
-            Load += Form1_Load;
-
-            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(800, 450);
-            Text = "LibVLCSharp.WinForms";
-            _videoView.Dock = DockStyle.Fill;
-            Controls.Add(_videoView);
-            _videoView.MediaPlayer = _mp;
-
-            ((System.ComponentModel.ISupportInitialize)(_videoView)).EndInit();
-            ResumeLayout(false);
-        }
-
-        private void Form1_Load(object sender, System.EventArgs e)
-        {
-            _mp.Play(new Media(_libVLC, "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", Media.FromType.FromLocation));
-        }
-
-        #endregion
     }
 }
--- Samples/LibVLCSharp.WinForms.Sample/Form1.cs
+++ Samples/LibVLCSharp.WinForms.Sample/Form1.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibVLCSharp.Shared;
+using System;
 using System.Windows.Forms;
 
 namespace LibVLCSharp.WinForms.Sample
 {
     public partial class Form1 : Form
     {
+        LibVLC _libVLC;
+        VideoView _videoView;
+        MediaPlayer _mp;
+
         public Form1()
         {
-            InitializeComponent();
+            if (!DesignMode)
+                Core.Initialize();
+
+            _videoView = new VideoView();
+            ((System.ComponentModel.ISupportInitialize)_videoView).BeginInit();
+            SuspendLayout();
+
+            _libVLC = new LibVLC("-vv");
+            _mp = new MediaPlayer(_libVLC);
+            Load += Form1_Load;
+
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Text = "LibVLCSharp.WinForms";
+            _videoView.Dock = DockStyle.Fill;
+            Controls.Add(_videoView);
+            _videoView.MediaPlayer = _mp;
+
+            ((System.ComponentModel.ISupportInitialize)(_videoView)).EndInit();
+            ResumeLayout(false);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _mp.Play(new Media(_libVLC, "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", Media.FromType.FromLocation));
         }
     }
 }
