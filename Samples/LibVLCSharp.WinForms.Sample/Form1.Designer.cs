2259c95bd60122833b2fdd5e45270978f7f7efa2
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2019-01-29 23:32:29 +0100

    WinForms sample cleanup

--- Samples/LibVLCSharp.WinForms.Sample/Form1.Designer.cs
+++ Samples/LibVLCSharp.WinForms.Sample/Form1.Designer.cs
@@ -16,8 +16,49 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
+                this._mp?.Dispose();
+                this._libVLC?.Dispose();
             }
             base.Dispose(disposing);
         }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoView1.Location = new System.Drawing.Point(0, 0);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(800, 450);
+            this.videoView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.videoView1);
+            this.Name = "Form1";
+            this.Text = "LibVLCSharp.WinForms";
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VideoView videoView1;
     }
 }
--- Samples/LibVLCSharp.WinForms.Sample/Form1.cs
+++ Samples/LibVLCSharp.WinForms.Sample/Form1.cs
@@ -1,41 +1,33 @@
-﻿using LibVLCSharp.Shared;
-using System;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibVLCSharp.Shared;
 
 namespace LibVLCSharp.WinForms.Sample
 {
     public partial class Form1 : Form
     {
-        LibVLC _libVLC;
-        VideoView _videoView;
-        MediaPlayer _mp;
+        public LibVLC _libVLC;
+        public MediaPlayer _mp;
 
         public Form1()
         {
             if (!DesignMode)
             {
-                Core.Initialize(Directory.GetParent(typeof(LibVLC).Assembly.Location).Parent.FullName);
+                Core.Initialize();
             }
 
-            _videoView = new VideoView();
-            ((System.ComponentModel.ISupportInitialize)_videoView).BeginInit();
-            SuspendLayout();
-
+            InitializeComponent();
             _libVLC = new LibVLC();
             _mp = new MediaPlayer(_libVLC);
+            this.videoView1.MediaPlayer = _mp;
             Load += Form1_Load;
-
-            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(800, 450);
-            Text = "LibVLCSharp.WinForms";
-            _videoView.Dock = DockStyle.Fill;
-            Controls.Add(_videoView);
-            _videoView.MediaPlayer = _mp;
-
-            ((System.ComponentModel.ISupportInitialize)_videoView).EndInit();
-            ResumeLayout(false);
         }
 
         private void Form1_Load(object sender, EventArgs e)
